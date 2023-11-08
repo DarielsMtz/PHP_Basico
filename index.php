4f9d43c7f59900dfc783c56b58ad3e06ea89812e
Dariels Mtz <darielsmartinez926@gmail.com>
2023-11-08 20:14:57 +0100

    Continuacion de los Conceptos Basicos en PHP

--- index.php
+++ index.php
@@ -147,3 +147,139 @@ echo "<br>" . ucfirst($oracion);
 echo "<br>" . ucwords($oracion);
 echo "<br>" . strtoupper($oracion);
 echo "<br>" . strtolower($oracion);
+
+// ---------------------------
+// Miercoles 08/11/23
+// Arrays y Matrices
+// ---------------------------
+
+// Crear una array
+// Usar funcion Array()
+
+$lista = array('verduras', 'carne', 'fruta');
+//                  0          1        2    ...
+$lista2 = array(10 => 'verduras', 20 => 'carne', 30 => 'fruta');
+//                      10                  20             30
+
+$lista3 = array(
+    2 => 'verduras',
+    4 => 'carne',
+    6 => 'fruta'
+);
+
+// Recoremos el array $lista3
+foreach ($lista3  as $indice => $valor) {
+    echo "$indice =  $valor";
+}
+
+// -------------------------------
+// Añadir un elemento a un array()
+// -------------------------------
+
+/*  
+Si no indicamos el indice en donde colocarlo
+lo añade al final siguiendo la numeracion de los indices 
+*/
+$lista[] = 'pasar';  // indice 3
+$lista[] = 'pescado'; // indice 4
+
+// Recoremos el array $lista
+foreach ($lista  as $indice => $valor) {
+    echo "$indice =  $valor";
+}
+
+$lista2[] = 'pasta'; // indice 31
+$lista2[0] = 'pescado'; // indice 0
+
+// Como imprimir un array()
+print_r($lista2);
+
+// Eliminar un elemento de un array()
+unset($lista2[0]);
+
+// Borrar todos los elementos de $lista2
+$lista2 = array();
+
+// Combinar arrays
+$array1 = array('verdura', 'carne', 'fruta');
+$array2 = array('pasta', 'pescado');
+
+$array3 = array_merge($array1, $array2);
+echo $array3;
+
+// Matrices (Arrays de 2 dimensiones)
+$matriz = array(
+    array(1, 2, 3),
+    array(4, 5, 6),
+    array(7, 8, 9)
+);
+
+/* 
+    1 2 3 
+    4 5 6
+    7 8 9
+
+*/
+// $matriz[fila][columna]
+echo $matriz[2][1]; // 8
+
+// Mostrar todos los campos de la matriz
+for ($i = 0; $i < count($matriz); $i++) {
+    for ($j = 0; $j < count($matriz[$i]); $j++) {
+        $valor = $matriz[$i][$j];
+        echo "matriz[$i][$j] = $valor <br/>";
+    }
+}
+
+// ---------------------------
+// Metodos para ordenar arrays
+// ---------------------------
+
+// Metodo sort() - ordena alfabeticamente y no mantiene los indices
+$alumno = array('patricia', 'juan', 'marta', 'alex', 'gerardo');
+sort($alumno);
+print_r($alumno);
+
+// Metodo rsort()
+// ordena de forma inversa y no mantiene los indices
+$alumno = array('patricia', 'juan', 'marta', 'alex', 'gerardo');
+rsort($alumno);
+print_r($alumno);
+
+// Metodo asort()
+// ordena alfabeticamente y mantiene los indices de valor
+$alumno = array('patricia', 'juan', 'marta', 'alex', 'gerardo');
+asort($alumno);
+print_r($alumno);
+
+// Metodo asort()
+// ordena y mantiene los indices de valor
+$alumno = array('patricia', 'juan', 'marta', 'alex', 'gerardo');
+arsort($alumno);
+print_r($alumno);
+
+// Metodo ksort()
+// ordena los indices y mantiene la correlacion indice => valor
+$alumno = array(2 => 'patricia', 1 => 'juan', 0 => 'marta', 4 => 'alex', 3 => 'gerardo');
+ksort($alumno);
+print_r($alumno);
+
+// Metodo krsort()
+// ordena de forma inveesa y mantiene los indices de valor
+$alumno = array('patricia', 'juan', 'marta', 'alex', 'gerardo');
+krsort($alumno);
+print_r($alumno);
+
+// ----------------------------------------
+// TRANSFORMACIONES ENTRE CADENAS Y ARRAYS
+// ----------------------------------------
+
+// implode() => convierte un array en un cadena
+$datos = array('imagen', 'email', 'telefono');
+$cadena_datos = implode(' - ', $datos);
+echo $cadena_datos;
+
+// explode() => convierte una cadena en un array
+$valores = "lunes-martes.miercoles";
+$array_valores = explode(' - ', $valores);
+echo $array_valores;
